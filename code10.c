25c5c88875d881a1eed33f67910d51304734b583
Divyaraj Singh bhati <divyarajsinghbhati0@gmail.com>
2026-08-14 10:53:06 +0530

    Add files via upload

---
+++ code10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+ {
+    int totalSeconds, hours, minutes, seconds;
+
+    printf("Enter total seconds: ");
+    scanf("%d", &totalSeconds);
+
+    hours = totalSeconds / 3600;
+    minutes = (totalSeconds % 3600) / 60;
+    seconds = totalSeconds % 60;
+
+    printf("%d seconds = %d hours, %d minutes, %d seconds\n",
+           totalSeconds, hours, minutes, seconds);
+
+    return 0;
+}
